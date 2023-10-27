756b1a4366d214cba105d5201ad31069395820fb
pratham0039 <98754411+pratham0039@users.noreply.github.com>
2023-10-27 23:40:05 +0530

    Update settings.py

--- FilmPro/settings.py
+++ FilmPro/settings.py
@@ -139,8 +139,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+
 
 
